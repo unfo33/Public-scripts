a3e506d727c2e859248447aa32ea9e304c24a775
Tom Bartlett <unfo33@gmail.com>
2022-04-07 15:31:17 -0400

    switched to requests, checked for update availability, icon check, optimization

--- macOS_Update_Prompt.py
+++ macOS_Update_Prompt.py
@@ -1,52 +1,57 @@
 #!/Library/ManagedFrameworks/Python/Python3.framework/Versions/Current/bin/python3
 
-import urllib.request
+import requests
 import datetime
 import platform
 import os
 import json
-import ssl
 import subprocess
+from os.path import exists
 
 # add major OS logic
-# optimize code to save line
-# add logic to ensure update is actually available
-
-# open software update pane
-cmd = "open -b com.apple.systempreferences /System/Library/PreferencePanes/SoftwareUpdate.prefPane"
-def run_cmd(cmd):
-    """Run the cmd"""
-    run = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, err = run.communicate()
-    if err:
-        print(err.decode("utf-8"))
-    return output, err
 
 # get info from apple
 URL = "https://gdmf.apple.com/v2/pmv"
-gcontext = ssl.SSLContext()
-raw = urllib.request.urlopen(URL, context=gcontext).read()
-list = json.loads(raw.decode("utf-8"))
-# grab latest version of Monty and posting date
+r = requests.get(URL, verify=False)
+list = r.json()
 macOS_Latest = list["AssetSets"]["macOS"][0]["ProductVersion"]
 posting_Date_STR = list["AssetSets"]["macOS"][0]["PostingDate"]
 # convert to date and give 7 day grace period
 posting_Date = datetime.datetime.strptime(posting_Date_STR, '%Y-%m-%d')
-one_Week = datetime.timedelta(days=7)
 start_Date = posting_Date + datetime.timedelta(days=7)
 final_Date = posting_Date + datetime.timedelta(days=30)
 infolink="https://support.apple.com/en-au/HT201222"
 today = datetime.date.today()
-days_Left = (final_Date.date() - today).days
-days_Left = int(days_Left)
+days_Left = int((final_Date.date() - today).days)
+# get current OS version
 current_OS = platform.mac_ver()[0]
 dialog="/usr/local/bin/dialog"
 
 def run_dialog(contentDict):
+    open_SU = "open -b com.apple.systempreferences /System/Library/PreferencePanes/SoftwareUpdate.prefPane"
     """Runs the SwiftDialog app and returns the exit code"""
     jsonString = json.dumps(contentDict)
-    exit_code = os.system(f"{dialog} --jsonstring '{jsonString}' --button1shellaction '{cmd}'")
+    exit_code = os.system(f"{dialog} --jsonstring '{jsonString}' --button1shellaction '{open_SU}'")
     return exit_code
+
+def icon_Check():
+    icon_Path = "/Library/Application Support/Dialog/VentureWell_logo_mark.png"
+    if os.path.exists(icon_Path):
+        pass
+    else:
+        r = requests.get("https://raw.githubusercontent.com/unfo33/venturewell/main/VentureWell_logo_mark.png")
+        open(icon_Path, 'wb').write(r.content)
+
+def update_Check():
+    # consider adding kickstart
+    # check for updates
+    update = ["softwareupdate", "-l"]
+    test = subprocess.Popen(update, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = test.communicate()
+    if result[1] == b'No new software available.\n':
+        return False
+    else:
+        return True
 
 # check type of update
 if current_OS[0:1] == macOS_Latest[0:1]:
@@ -59,6 +64,7 @@ elif current_OS[0:1] != macOS_Latest[0:1]:
     Update = "major"
     time = "60+"
 
+# main dialog content
 content_dict = {
     "alignment": "center",
     "button1text": "Update Now",
@@ -75,28 +81,23 @@ content_dict = {
     "moveable": 1,
 }
 
-final_content_dict = {
-    "alignment": "center",
-    "button1text": "Update Now",
-    "centericon": 1,
-    "icon": "/Library/Application Support/Dialog/VentureWell_logo_mark.png",
-    "infobuttonaction": infolink,
-    "infobuttontext": "More Info",
-    "message": f"## Operating System Update Required\n\nYour Update Path: **{current_OS}** → **{macOS_Latest}**\n\nmacOS **{macOS_Latest}** was released on **{posting_Date_STR}**. It is a **{Update}** update and will require around **{time}** minutes downtime.\n\nDays Remaining to Update: **{days_Left}**\n\n*To begin the update, click on **Update Now** and follow the provided steps.*\n*You can also use the [Mac Manage App](https://docs.google.com/document/d/1oWuT7Tgsv-DHFNmivSc_Ibz61vtKKkRvqQoYmQsrKzI/edit?usp=sharing) to update at your convenience*.",
-    "messagefont": "size=16",
-    "title": "none",
-    "height": "420",
-    "width": "900",
-    "ontop": 1,
-    "moveable": 1,
-}
+icon_Check()
+
 if current_OS == macOS_Latest:
     print("On Latest Version")
 else:
-    if days_Left <= 23:
-        while days_Left == 0:
-            run_dialog(final_content_dict)
+    if update_Check():
+        if days_Left <= 23:
+            message = {"message": f"## Operating System Update Required\n\nYour Update Path: **{current_OS}** → **{macOS_Latest}**\n\nmacOS **{macOS_Latest}** was released on **{posting_Date_STR}**. It is a **{Update}** update and will require around **{time}** minutes downtime.\n\nDays Remaining to Update: **{days_Left}**\n\n*To begin the update, click on **Update Now** and follow the provided steps.*\n*You can also use the [Mac Manage App](https://docs.google.com/document/d/1oWuT7Tgsv-DHFNmivSc_Ibz61vtKKkRvqQoYmQsrKzI/edit?usp=sharing) to update at your convenience*."}
+            del content_dict["button2text"]
+            ontop = {"ontop": 1}
+            content_dict.update(ontop)
+            content_dict.update(message)
+            while days_Left == 0:
+                run_dialog(content_dict)
+            else:
+                run_dialog(content_dict)
         else:
-            run_dialog(content_dict)
+            print("still in grace period")
     else:
-        print("still in grace period")
+        print ("New Update available on Apple website but not locally")
